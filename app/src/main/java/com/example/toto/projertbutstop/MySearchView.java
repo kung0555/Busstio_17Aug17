d38fdcd283341dd79ec72679545f7dca733fc31a
MasterUNG <phrombutr@gmail.com>
2017-08-18 12:56:53 +0700

    Search Success

--- app/src/main/java/com/example/toto/projertbutstop/MySearchView.java
+++ app/src/main/java/com/example/toto/projertbutstop/MySearchView.java
@@ -1,9 +1,12 @@
 package com.example.toto.projertbutstop;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
-import android.widget.SearchView;
+
 
 import java.util.ArrayList;
 
@@ -23,9 +26,11 @@ public class MySearchView extends AppCompatActivity implements android.widget.Se
 
         // Generate sample data
 
-        animalNameList = new String[]{"Lion", "Tiger", "Dog",
-                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
-                "Cow","Donkey","Monkey"};
+//        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+//                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+//                "Cow","Donkey","Monkey"};
+
+        animalNameList = getBusStopFromServer();
 
         // Locate the ListView in listview_main.xml
         list = (ListView) findViewById(R.id.listview);
@@ -45,6 +50,36 @@ public class MySearchView extends AppCompatActivity implements android.widget.Se
         // Locate the EditText in listview_main.xml
         editsearch = (android.widget.SearchView) findViewById(R.id.search);
         editsearch.setOnQueryTextListener(MySearchView.this);
+    }
+
+    private String[] getBusStopFromServer() {
+
+        String tag = "18AugV2";
+        String[] resultStrings = new String[0];
+
+        try {
+
+            SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                    MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM busstopTABLE", null);
+            cursor.moveToFirst();
+
+            resultStrings = new String[cursor.getCount()];
+
+            for (int i=0;i<resultStrings.length; i+=1) {
+                resultStrings[i] = cursor.getString(3);
+                Log.d(tag, "result[" + i + "] ==> " + resultStrings[i]);
+                cursor.moveToNext();
+            }
+
+            return resultStrings;
+        } catch (Exception e) {
+            Log.d(tag, "e ==> " + e.toString());
+
+        }
+
+
+        return resultStrings;
     }
 
     @Override
